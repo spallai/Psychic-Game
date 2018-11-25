589961c4cb21b122ae7c0117b53179340a8cc5be
SPallai <sangeeta.sumair@gmail.com>
2018-11-25 14:16:06 -0800

    updated javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wins = 0;
+var losses = 0;
+var guessesleft = 10;
+var computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log("Computer chose " + computerSelection);
+
+document.onkeyup = function (event) {
+
+    var userGuess = event.key.toLowerCase();
+
+    if (computerChoices.includes(userGuess)) {
+        if (userGuess === computerSelection) {
+            wins++;
+            document.getElementById("wins").textContent = wins;
+            reset();
+        }
+        else if (guessesleft > 1) {
+            guessesleft--;
+            document.getElementById("guesses-left").textContent = guessesleft;
+            console.log("You have this many guesses left " + guessesleft);
+            document.getElementById("user-guess").textContent += " " + userGuess;
+        }
+        else {
+            losses++;
+            document.getElementById("losses").textContent = losses;
+            reset();
+        }
+        
+        function reset() {
+            guessesleft = 10;
+            computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            console.log("Computer chose " + computerSelection);
+            document.getElementById("user-guess").textContent = "";
+            document.getElementById("guesses-left").textContent = guessesleft;
+        }
+    }
+};
+
